da37a34b80452cc99bf8b950cb37f76a2caaaf24
Arthur Vincent Simon <asimon@innovuze.com>
2016-09-07 09:51:33 +0800

    Reverse and print the inputted array

---
+++ all-domains/data-structures/arrays/arrays-ds/solution.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/arrays-ds
+# Python 2
+
+length = raw_input()
+numbers_input = raw_input()
+numbers = numbers_input.split(' ')
+
+print(' '.join(numbers[::-1]))
