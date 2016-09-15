de29ba78eb7719de5cdffe5556998ab6dbb1e395
Arthur Vincent Simon <developarvin@gmail.com>
2016-09-15 13:50:03 +0800

    Print elements of linked list in reverse

---
+++ all-domains/data-structures/linked-lists/print-in-reverse/solution.py
@@ -0,0 +1,22 @@
+# https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list-in-reverse
+# Python 2
+
+"""
+ Print elements of a linked list in reverse order as standard output
+ head could be None as well for empty list
+ Node is defined as
+
+ class Node(object):
+
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+
+"""
+
+def ReversePrint(head):
+    if head == None:
+        return None
+    ReversePrint(head.next)
+    print(head.data)
