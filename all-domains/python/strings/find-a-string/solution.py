5614173e450295d1b0b65acd11324d5b99354869
Arthur Vincent Simon <developarvin@gmail.com>
2018-02-07 08:13:46 +0800

    Initial scaffolding of files

---
+++ all-domains/python/strings/find-a-string/solution.py
@@ -0,0 +1,9 @@
+#!/usr/env/bin python
+# https://www.hackerrank.com/challenges/find-a-string/problem
+# Python 3
+
+def find_a_string(string, substring):
+    print(string)
+    print(substring)
+    return 2
+
---
+++ all-domains/python/strings/find-a-string/test_solution.py
@@ -0,0 +1,4 @@
+from solution import find_a_string
+
+def test_find_a_string():
+    assert(find_a_string('ABCDCDC', 'CDC') == 2)
